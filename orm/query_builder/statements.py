992e83bc5d08bef394bd9b2d92bf250e3c7b5ef4
Giovanni Elias <giovanni.elias@flowe.com>
2023-07-22 18:50:47 +0200

    added base statements

---
+++ orm/query_builder/statements.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+CREATE_TABLE_BASE_STMT = 'CREATE TABLE [IF NOT EXISTS] {table_name} ({column_definitions});'
+INSERT_BASE_STMT = 'INSERT INTO {table_name} ({columns}) VALUES ({values});'
+SELECT_BASE_STMT = 'SELECT {columns} FROM {table_name}'
+DELETE_BASE_STMT = 'DELETE FROM {table_name}'
+WHERE_CLAUSE = 'WHERE {conditions}'
+GROUP_BY_CLAUSE = 'GROUP BY {columns}'
+ORDER_BY_CLAUSE = 'ORDER BY {columns}'
+ASCENDING_ORDER = 'ASC'
+DESCENDING_ORDER = 'DESC'
+AND = 'AND'
